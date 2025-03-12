ac66615d4c00b0e215a8ba2c3196de30a8adbb78
chihebmezrigui1 <mezrigui.chihab@gmail.com>
2025-03-12 11:10:16 +0100

    Gestion de profil DONE

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -60,7 +60,6 @@ const Post = ({ post }) => {
 			}
 		},
 		onSuccess: (updatedLikes) => {
-			// this is not the best UX, bc it will refetch all posts
 			// queryClient.invalidateQueries({ queryKey: ["posts"] });
 
 			// instead, update the cache directly for that post
